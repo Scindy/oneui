aa6cf8c391a22d3fb692d52a34f2fef2cefcf42d
mozi <mozi@mozibrand.com>
2019-11-01 16:39:38 +0800

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+	printWidth: 160, //一行的字符数，如果超过会进行换行，默认为80
+	tabWidth: 2, // tab缩进大小,默认为2
+	useTabs: true, // 使用tab缩进，默认false
+	singleQuote: true, // 使用单引号, 默认false(在jsx中配置无效, 默认都是双引号)
+	semi: true, // 使用分号, 默认true
+	// 对象中的空格 默认true
+	// true: { foo: bar }
+	// false: {foo: bar}
+	bracketSpacing: true, //对象大括号直接是否有空格，默认为true，效果：{ foo: bar }
+	// JSX标签闭合位置 默认false
+	// false: <div
+	//          className=""
+	//          style={{}}
+	//       >
+	// true: <div
+	//          className=""
+	//          style={{}} >
+	jsxBracketSameLine: false,
+	// 箭头函数参数括号 默认avoid 可选 avoid| always
+	// avoid 能省略括号的时候就省略 例如x => x
+	// always 总是有括号
+	arrowParens: 'avoid'
+};
